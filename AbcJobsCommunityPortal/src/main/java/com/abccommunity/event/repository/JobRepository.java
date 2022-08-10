aaf074946a427421a193e44bb88229148558f5f2
V-Zlfkr <96854104+V-Zlfkr@users.noreply.github.com>
2022-08-11 00:43:12 +0800

    Delete AbcJobsCommunityPortal directory

--- AbcJobsCommunityPortal/src/main/java/com/abccommunity/event/repository/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abccommunity.event.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.abccommunity.event.dto.JobDto;
-
-public interface JobRepository extends JpaRepository<JobDto, Integer>{
-
-}
